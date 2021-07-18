4c59d63ad57cc801f423cda97ac660e83e2f9fa5
mathieupelle <mathieu.pelle7@gmail.com>
2021-07-18 15:26:46 +0200

    Now able to plot polars at different radial locations and for different cases

--- A2_Dynamic Stall/Plotting_func.py
+++ A2_Dynamic Stall/Plotting_func.py
@@ -13,14 +13,14 @@ plt.rc('font', family='serif')
 def Plotting_following_blade(res,mod_labels,rad_pos = [0.4,0.6,0.8],lims=False):
     var = ['alpha','phi','a','ap','f_tan','f_nor','cl']
     labels = [r'$\alpha$ [deg]','$\phi$ [deg]', 'a [-]','$a^,[-]$', '$C_t$ [-]', '$C_n$ [-]', '$C_{l}$ [-]']
-    
+
     cols = ['tab:blue','tab:orange','tab:green']
-        
+
     #Get the index of the wanted radial positions
     idx_mu = np.zeros(len(rad_pos))
     for i,val in enumerate(rad_pos):
         idx_mu[i] = (np.abs(res[0].mu[:,0,0] - val)).argmin()
-    
+
     # Depends on the number of inputs that we pass
     for i,par in enumerate(var):
         plt.figure()
@@ -35,13 +35,13 @@ def Plotting_following_blade(res,mod_labels,rad_pos = [0.4,0.6,0.8],lims=False):
                     az_idx = az_idx+1
                 else:
                     az_idx = 0
-            
+
             l0, = plt.plot(res[j].time,x[0,:],color=cols[j])
             line_handles.append(l0)
             try:
                 l1, = plt.plot(res[j].time,x[1,:],'--',color=cols[j])
                 try:
-                    l2, = plt.plot(res[j].time,x[2,:],'-.',color=cols[j])   
+                    l2, = plt.plot(res[j].time,x[2,:],'-.',color=cols[j])
                     sec_leg_labels = list(np.round_(res[j].mu[idx_mu.astype(int),0,0],2))
                     sec_legend = plt.legend(handles=[l0,l1,l2], labels= sec_leg_labels)
                     # Add the legend manually to the current Axes.
@@ -51,10 +51,10 @@ def Plotting_following_blade(res,mod_labels,rad_pos = [0.4,0.6,0.8],lims=False):
                     # Add the legend manually to the current Axes.
             except:
                 pass
-            
-            
+
+
                 # Create another legend for the second line.
-                #plt.legend(handles=[line2], loc='lower right')          
+                #plt.legend(handles=[line2], loc='lower right')
         plt.xlabel('Time [s]')
         plt.ylabel(labels[i])
         plt.grid()
@@ -67,19 +67,19 @@ def Plotting_following_blade(res,mod_labels,rad_pos = [0.4,0.6,0.8],lims=False):
             plt.xlim(lims)
         plt.show()
 
-        
+
 
 def Plotting_integral_quantities(res,mod_labels,lims=False):
     var = ['CT','CP']
     labels=[r'$C_T$',r'$C_P$']
-    
+
     for i,par in enumerate(var):
         plt.figure()
         for j in range(len(res)):
             Z =getattr(res[j],par)
             plt.plot(res[j].time,Z,label=mod_labels[j])
-            
-            
+
+
         plt.xlabel('Time [s]')
         plt.ylabel(labels[i])
         plt.grid()
@@ -87,3 +87,47 @@ def Plotting_integral_quantities(res,mod_labels,lims=False):
         if lims:
             plt.xlim(lims)
         plt.show()
+
+
+def PLotting_polars(res, rad_pos, omega):
+
+    time = res[0].time
+    T = 2*np.pi/omega
+    idx1 = np.argmin(np.abs(time-2*T))
+    idx2 = np.argmin(np.abs(time-3*T))+1
+
+    idx_mu = np.zeros(len(rad_pos))
+    for i,val in enumerate(rad_pos):
+        idx_mu[i] = (np.abs(res[0].mu[:,0,0] - val)).argmin()
+
+    for j in range(len(res)):
+        plt.figure()
+        plt.grid()
+        plt.xlabel(r'$\alpha$ [deg]')
+        plt.ylabel('Cn [-]')
+        if j>0:
+            Z = res[j].UA_class.Cn
+        else:
+            Z = res[j].cl
+
+        x = np.zeros((len(rad_pos),len(res[j].time)))
+        az_idx = 0
+        for k in range(x.shape[1]):
+            x[:,k] = Z[idx_mu.astype(int),az_idx,k]
+            if az_idx+1 < Z.shape[1]:
+                az_idx = az_idx+1
+            else:
+                az_idx = 0
+
+        a = np.zeros((len(rad_pos),len(res[j].time)))
+        az_idx = 0
+        for k in range(a.shape[1]):
+            a[:,k] = res[j].alpha[idx_mu.astype(int),az_idx,k]
+            if az_idx+1 < res[j].alpha.shape[1]:
+                az_idx = az_idx+1
+            else:
+                az_idx = 0
+
+        for l in range(len(rad_pos)):
+            plt.plot(a[l,idx1:idx2],x[l,idx1:idx2], label='$\mu$='+str(rad_pos[l]))
+        plt.legend()
